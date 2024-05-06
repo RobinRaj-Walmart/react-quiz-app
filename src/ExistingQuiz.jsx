bddd011f0f74c61b9a92faa9d81834bc0cfacf4e
Robin Raj <robin.raj@walmart.com>
2024-05-06 18:26:37 +0530

    added score calculation

--- src/ExistingQuiz.jsx
+++ src/ExistingQuiz.jsx
@@ -4,25 +4,44 @@ export default function ExistingQuiz({goBack, users, quizes}) {
     const [showQuiz, setShowQuiz] = useState(0);
     const [submit, setSubmit] = useState(0);
     const [currentIndex, setCurrentIndex] = useState(0);
+    const [answers, setAnswers] = useState([]);
+    const [currentAns, setCurrentAns] = useState('');
+    const [score, setScore] = useState(0);
 
     const timeForEachQues = 10000;
 
+    function handleSubmit() {
+      if(currentIndex===quizes[quizid].questions.length-1) {
+        setSubmit(1);
+        setShowQuiz(2);
+      }
+      else {
+        setSubmit(1);
+      }
+    }
+
     useEffect(()=>{
-        if(submit===1) {
-            console.log("moving to the next question");
-            if(currentIndex<quizes[quizid].questions.length-1){
-                console.log("value of current index has been increased");
-                setCurrentIndex(currentIndex+1);
-            }
-            setSubmit(0);
-        }
+      if(submit===1) {
+          setAnswers([...answers, currentAns]);
+          if(currentAns===quizes[quizid].answers[currentIndex])
+            setScore(score+1);
+          setCurrentAns('');
+          console.log("moving to the next question");
+          if(currentIndex<quizes[quizid].questions.length-1){
+              console.log("value of current index has been increased");
+              setCurrentIndex(currentIndex+1);
+          }
+          setSubmit(0);
+      }
     }, [submit])
 
     useEffect(()=> { 
       let tempIndex = currentIndex;
       const timeoutId = setTimeout(()=>{
         if(currentIndex===tempIndex && currentIndex<quizes[quizid].questions.length-1) {
+          setAnswers([...answers, '']);
           setCurrentIndex(currentIndex+1);
+          setCurrentAns('');
           console.log("currentIndex:"+currentIndex + " tempIndex: "+tempIndex);
         }
       }, timeForEachQues);
@@ -38,17 +57,24 @@ export default function ExistingQuiz({goBack, users, quizes}) {
           <button onClick={()=>setShowQuiz(1)} className='border border-black p-2 m-2'>Open my quiz</button>
           <button className='border' onClick={goBack}>Move to home</button>
           <br />
-          {showQuiz && 
+          {showQuiz===1 && 
             (
               <>
-                  <div className="flex gap-3 items-center justify-center">
-                      <div>{quizes[quizid].questions[currentIndex]}</div>
-                      <input type="text" placeholder="Enter your answer here" className="border" />
-                      <button className="border" onClick={()=>setSubmit(1)}>
-                          {currentIndex!==quizes[quizid].questions.length-1?(<div>Submit and move to next question</div>):(<div>Final Submit</div>)}
-                      </button>
-                  </div>
-                  <br />
+                <div className="flex gap-3 items-center justify-center">
+                    <div>{quizes[quizid].questions[currentIndex]}</div>
+                    <input value={currentAns} onChange={(e)=>setCurrentAns(e.target.value)} type="text" placeholder="Enter your answer here" className="border" />
+                    <button className="border" onClick={handleSubmit}>
+                        {currentIndex!==quizes[quizid].questions.length-1?(<div>Submit and move to next question</div>):(<div>Final Submit</div>)}
+                    </button>
+                </div>
+                <br />
+              </>
+            )
+          }
+          {showQuiz===2 && 
+            (
+              <>
+                <div>Your Score: {score}</div>
               </>
             )
           }
