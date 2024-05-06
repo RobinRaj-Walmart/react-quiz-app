d5d7edb6eaa6bcc4401b9d1df741c8c793136bc3
Robin Raj <robin.raj@walmart.com>
2024-05-06 20:04:45 +0530

    some more ui changes

--- src/Home.jsx
+++ src/Home.jsx
@@ -41,13 +41,19 @@ export default function Home() {
     return (
       <>
         {mode===0 && (
-          <div className='flex h-screen bg-gray-300 justify-center items-center'>
-            <button onClick={()=>setMode(1)} className='p-10 border-8 rounded m-10 bg-cyan-300 font-bold'>
-              Create a new Quiz
-            </button>
-            <button onClick={()=>setMode(2)} className='p-10 border-8 rounded m-10 bg-cyan-300 font-bold'>
-              Give an existing quiz
-            </button>
+          
+          <div className='flex h-screen flex-col bg-gray-300 justify-center items-center'>
+            <div className="text-8xl font-sans font-bold text-stone-700">Quiz App</div>
+            <br />
+            <br />
+            <div>
+              <button onClick={()=>setMode(1)} className='p-10 border-8 rounded m-10 bg-cyan-300 font-bold'>
+                Create a new Quiz
+              </button>
+              <button onClick={()=>setMode(2)} className='p-10 border-8 rounded m-10 bg-cyan-300 font-bold'>
+                Give an existing quiz
+              </button>
+            </div>
           </div>
         )}
         {mode===1 && <CreateQuiz goBack={resetMode} addNewUser={addUser} addQuiz={addQuiz} />}
